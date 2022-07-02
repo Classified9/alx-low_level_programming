e4ce431b3986201d3636809d34115b52c770a0b3
Sherif Awofiranye <classifiedeveloper@gmail.com>
2022-07-01 20:41:38 -0700

    2-print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
-
 
 /**
  *
@@ -11,14 +8,13 @@
  * *Return: 0 if successful
  *
  * */
-
 int main(void)
 {
 	char var;
 	
 	for (var = 'a'; var <= 'z'; var++)
 	{
-	putchar(var);
+		putchar(var);
 	}
 	putchar('\n');
 	return (0);
