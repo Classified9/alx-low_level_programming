a93bc83d4cc9de14155343906054e323d6de3e58
Sherif Awofiranye <classifiedeveloper@gmail.com>
2022-07-02 10:39:54 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,28 +1,12 @@
 #include <stdio.h>
-
-#include <string.h>
-
-
+#include <string.h> 
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *
+ * main - main block
+ * Return 0
  */
 
-
 int main(void)
-
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
-	 
-
 	return(0);
-
 }
