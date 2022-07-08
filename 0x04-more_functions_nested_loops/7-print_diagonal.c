c2b7091e9ceaabcc504e0cf3af951b7bec48555c
Sherif Awofiranye <classifiedeveloper@gmail.com>
2022-07-07 17:52:50 -0700

    functions and nested loops

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,183 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal (_) n number of times
+ * @n: integer variable
+ *
+ * Created by: Jonah Jockthan
+ * cc: 7th july, 2022
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < n; i++)
+		_putchar('_');
+	_putchar('\n');
+}#include "main.h"
+
+/**
+ *  * print_diagonal - print diagonal (_) n number of times
+ *   * @n: integer variable
+ *    *
+ *     * Created by: Jonah Jockthan
+ *      * cc: 7th july, 2022
+ *       */
+
+void print_line(int n)
+{
+		int i;
+
+			if (n <= 0)
+			{
+						_putchar('\n');
+								return;
+			}
+
+				for (i = 0; i < n; i++)
+							_putchar('_');
+					_putchar('\n');
+}#include "main.h"
+
+/**
+ *  * print_diagonal - print diagonal (_) n number of times
+ *   * @n: integer variable
+ *    *
+ *     * Created by: Jonah Jockthan
+ *      * cc: 7th july, 2022
+ *       */
+
+void print_line(int n)
+{
+		int i;
+
+			if (n <= 0)
+			{
+						_putchar('\n');
+								return;
+			}
+
+				for (i = 0; i < n; i++)
+							_putchar('_');
+					_putchar('\n');
+}#include "main.h"
+
+/**
+ *  * print_diagonal - print diagonal (_) n number of times
+ *   * @n: integer variable
+ *    *
+ *     * Created by: Jonah Jockthan
+ *      * cc: 7th july, 2022
+ *       */
+
+void print_line(int n)
+{
+		int i;
+
+			if (n <= 0)
+			{
+						_putchar('\n');
+								return;
+			}
+
+				for (i = 0; i < n; i++)
+							_putchar('_');
+					_putchar('\n');
+}#include "main.h"
+
+/**
+ *  * print_diagonal - print diagonal (_) n number of times
+ *   * @n: integer variable
+ *    *
+ *     * Created by: Jonah Jockthan
+ *      * cc: 7th july, 2022
+ *       */
+
+void print_line(int n)
+{
+		int i;
+
+			if (n <= 0)
+			{
+						_putchar('\n');
+								return;
+			}
+
+				for (i = 0; i < n; i++)
+							_putchar('_');
+					_putchar('\n');
+}#include "main.h"
+
+/**
+ *  * print_diagonal - print diagonal (_) n number of times
+ *   * @n: integer variable
+ *    *
+ *     * Created by: Jonah Jockthan
+ *      * cc: 7th july, 2022
+ *       */
+
+void print_line(int n)
+{
+		int i;
+
+			if (n <= 0)
+			{
+						_putchar('\n');
+								return;
+			}
+
+				for (i = 0; i < n; i++)
+							_putchar('_');
+					_putchar('\n');
+}#include "main.h"
+
+/**
+ *  * print_diagonal - print diagonal (_) n number of times
+ *   * @n: integer variable
+ *    *
+ *     * Created by: Jonah Jockthan
+ *      * cc: 7th july, 2022
+ *       */
+
+void print_line(int n)
+{
+		int i;
+
+			if (n <= 0)
+			{
+						_putchar('\n');
+								return;
+			}
+
+				for (i = 0; i < n; i++)
+							_putchar('_');
+					_putchar('\n');
+}#include "main.h"
+
+/**
+ *  * print_diagonal - print diagonal (_) n number of times
+ *   * @n: integer variable
+ *    *
+ *       */
+
+void print_line(int n)
+{
+		int i;
+
+			if (n <= 0)
+			{
+						_putchar('\n');
+								return;
+			}
+
+				for (i = 0; i < n; i++)
+							_putchar('_');
+					_putchar('\n');
+}
