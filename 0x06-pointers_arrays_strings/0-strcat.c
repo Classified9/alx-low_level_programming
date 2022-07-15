735e8de5e64a328d8d7c5f59ca0a1ba8f05b684b
Sherif Awofiranye <classifiedeveloper@gmail.com>
2022-07-14 21:06:01 -0700

    Pointers arrays and Strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat - a function that concatenates two strings
+ * @dest: copy to
+ * @src: copy from
+ * Return: two concat strings
+ */
+char *_strcat(char *dest, char *src)
+{
+	int destLen = 0;
+	int srcLen = 0;
+
+	while (dest[destLen] != '\0')
+		destLen++;
+
+	while (src[srcLen])
+	{
+		dest[destLen] = src[srcLen];
+		srcLen++;
+		destLen++;
+	}
+	dest[destLen] = '\0';
+
+	return (dest);
+}
